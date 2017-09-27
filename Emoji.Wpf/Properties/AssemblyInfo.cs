d9c3d0346481cb4261ab6e24b574af87f88ed546
Sam Hocevar <sam@hocevar.net>
2017-09-27 13:12:08 +0200

    Add a Visual Studio solution and a sample program.

---
+++ Emoji.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿//
+//  Emoji.Wpf — Emoji support for WPF
+//
+//  Copyright © 2017 Sam Hocevar <sam@hocevar.net>
+//
+//  This library is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What the Fuck You Want
+//  to Public License, Version 2, as published by the WTFPL Task Force.
+//  See http://www.wtfpl.net/ for more details.
+//
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("emoji.wpf")]
+[assembly: AssemblyDescription("Emoji support for WPF applications")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("emoji.wpf")]
+[assembly: AssemblyCopyright("Copyright © 2017 Sam Hocevar")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6a9c0ad6-14f7-451a-a0d4-38987a275f0d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sample/SampleWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿//
+//  Emoji.Wpf — Emoji support for WPF
+//
+//  Copyright © 2017 Sam Hocevar <sam@hocevar.net>
+//
+//  This program is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What the Fuck You Want
+//  to Public License, Version 2, as published by the WTFPL Task Force.
+//  See http://www.wtfpl.net/ for more details.
+//
+
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for SampleWindow.xaml
+    /// </summary>
+    public partial class SampleWindow : Window
+    {
+        public SampleWindow()
+        {
+            InitializeComponent();
+
+            string text = "Hi🙌, I♥emojis☺\nEdit me!\n🍰✈✏📞☘️💩";
+            SampleTextBox.Document = new FlowDocument(new Paragraph(new Run(text)));
+        }
+    }
+}
