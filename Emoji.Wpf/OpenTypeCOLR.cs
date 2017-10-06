92429224a808010b5eb57463aa19779dd6c50a34
Sam Hocevar <sam@hocevar.net>
2017-10-06 13:44:57 +0200

    Split the font reader some more.

---
+++ Emoji.Wpf/OpenTypeCOLR.cs
@@ -0,0 +1,48 @@
+﻿//
+//  Emoji.Wpf — Emoji support for WPF
+//
+//  Copyright © 2017 Sam Hocevar <sam@hocevar.net>
+//
+//  This library is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What the Fuck You Want
+//  to Public License, Version 2, as published by the WTFPL Task Force.
+//  See http://www.wtfpl.net/ for more details.
+//
+
+namespace Emoji.Wpf
+{
+    public partial class ColorTypeface
+    {
+        // Read the COLR table
+        // https://www.microsoft.com/typography/otspec/colr.htm
+        private void ReadCOLR(BEBinaryReader b, int offset)
+        {
+            b.SeekAt(offset);
+
+            ushort version = b.ReadUInt16();
+            int glyph_count = b.ReadUInt16();
+            int glyphs_offset = b.ReadInt32();
+            int layers_offset = b.ReadInt32();
+            int layers_count = b.ReadUInt16();
+            m_glyph_layers = new ushort[layers_count];
+            m_glyph_palettes = new ushort[layers_count];
+
+            b.SeekAt(offset + glyphs_offset);
+            for (int i = 0; i < glyph_count; ++i)
+            {
+                ushort gid = b.ReadUInt16();
+                m_layer_indices[gid] = b.ReadUInt16();
+                m_layer_counts[gid] = b.ReadUInt16();
+            }
+
+            b.SeekAt(offset + layers_offset);
+            for (int i = 0; i < m_glyph_layers.Length; ++i)
+            {
+                m_glyph_layers[i] = b.ReadUInt16();
+                m_glyph_palettes[i] = b.ReadUInt16();
+            }
+        }
+    }
+}
+
--- Emoji.Wpf/OpenTypeGSUB.cs
+++ Emoji.Wpf/OpenTypeGSUB.cs
@@ -12,75 +12,11 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Windows.Media;
 
 namespace Emoji.Wpf
 {
-    public partial class ColorTypeface : Typeface
+    public partial class ColorTypeface
     {
-        /// <summary>
-        /// Big-endian binary reader for TrueType/OTF fonts.
-        /// </summary>
-        private class BEBinaryReader : BinaryReader
-        {
-            public BEBinaryReader(Stream s) : base(s) { }
-
-            public override short ReadInt16() => BitConverter.ToInt16(ReadReverse(2), 0);
-            public override ushort ReadUInt16() => BitConverter.ToUInt16(ReadReverse(2), 0);
-            public override int ReadInt32() => BitConverter.ToInt32(ReadReverse(4), 0);
-            public override uint ReadUInt32() => BitConverter.ToUInt32(ReadReverse(4), 0);
-            public override long ReadInt64() => BitConverter.ToInt64(ReadReverse(8), 0);
-            public override ulong ReadUInt64() => BitConverter.ToUInt64(ReadReverse(8), 0);
-
-            public ushort[] ReadUInt16Table(int count)
-            {
-                ushort[] ret = new ushort[count];
-                for (int i = 0; i < count; ++i)
-                    ret[i] = ReadUInt16();
-                return ret;
-            }
-
-            public string ReadTag() => Encoding.ASCII.GetString(ReadBytes(4));
-
-            public void SeekAt(int offset) => BaseStream.Seek(offset, SeekOrigin.Begin);
-
-            private byte[] ReadReverse(int count)
-            {
-                var b = ReadBytes(count); Array.Reverse(b); return b;
-            }
-        }
-
-        private void ReadFontStream(Stream s)
-        {
-            // Read font header
-            var b = new BEBinaryReader(s);
-            uint font_header = b.ReadUInt32();
-            int table_count = b.ReadUInt16();
-            b.SeekAt(12);
-
-            // Read available table information
-            Dictionary<string, int> tables = new Dictionary<string, int>();
-            for (int i = 0; i < table_count; ++i)
-            {
-                string tag = b.ReadTag();
-                uint checksum = b.ReadUInt32();
-                int offset = b.ReadInt32();
-                int length = b.ReadInt32(); // Ignored, lol
-                tables[tag] = offset;
-            }
-
-            // Parse tables that are relevant to us
-            int table_offset;
-            if (tables.TryGetValue("GSUB", out table_offset))
-                ReadGSUB(b, table_offset);
-            if (tables.TryGetValue("COLR", out table_offset))
-                ReadCOLR(b, table_offset);
-            if (tables.TryGetValue("CPAL", out table_offset))
-                ReadCPAL(b, table_offset);
-        }
-
         // Read the GSUB table
         // https://www.microsoft.com/typography/otspec/gsub.htm
         // https://www.microsoft.com/typography/otspec/chapter2.htm
@@ -310,60 +246,6 @@ namespace Emoji.Wpf
 
                 for (int i = 0; i < lookahead_offsets.Length; ++i)
                     lookaheads[i] = new Coverage(b, offset + lookahead_offsets[i]);
-            }
-        }
-
-        // Read the COLR table
-        // https://www.microsoft.com/typography/otspec/colr.htm
-        private void ReadCOLR(BEBinaryReader b, int offset)
-        {
-            b.SeekAt(offset);
-
-            ushort version = b.ReadUInt16();
-            int glyph_count = b.ReadUInt16();
-            int glyphs_offset = b.ReadInt32();
-            int layers_offset = b.ReadInt32();
-            int layers_count = b.ReadUInt16();
-            m_glyph_layers = new ushort[layers_count];
-            m_glyph_palettes = new ushort[layers_count];
-
-            b.SeekAt(offset + glyphs_offset);
-            for (int i = 0; i < glyph_count; ++i)
-            {
-                ushort gid = b.ReadUInt16();
-                m_layer_indices[gid] = b.ReadUInt16();
-                m_layer_counts[gid] = b.ReadUInt16();
-            }
-
-            b.SeekAt(offset + layers_offset);
-            for (int i = 0; i < m_glyph_layers.Length; ++i)
-            {
-                m_glyph_layers[i] = b.ReadUInt16();
-                m_glyph_palettes[i] = b.ReadUInt16();
-            }
-        }
-
-        // Read the CPAL table
-        // https://www.microsoft.com/typography/otspec/cpal.htm
-        private void ReadCPAL(BEBinaryReader b, int offset)
-        {
-            b.SeekAt(offset);
-
-            int version = b.ReadUInt16();
-            int entry_count = b.ReadUInt16(); // XXX: unused?
-            int palette_count = b.ReadUInt16();
-            m_colors = new Color[b.ReadUInt16()];
-            int colors_offset = b.ReadInt32();
-
-            m_palettes = b.ReadUInt16Table(palette_count);
-            for (int i = 0; i < m_palettes.Length; ++i)
-                m_palettes[i] = b.ReadUInt16();
-
-            b.SeekAt(offset + colors_offset);
-            for (int i = 0; i < m_colors.Length; ++i)
-            {
-                byte[] tmp = b.ReadBytes(4);
-                m_colors[i] = Color.FromArgb(tmp[3], tmp[2], tmp[1], tmp[0]);
             }
         }
     }
