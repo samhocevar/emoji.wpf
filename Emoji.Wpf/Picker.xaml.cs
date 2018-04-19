f5d39dc7e8ef2823afeedbafd53ba187b40532ee
Sam Hocevar <sam@hocevar.net>
2018-04-19 12:33:33 +0200

    New emoji picker class.
    Emoji are not sorted by category yet, but it’s a good start.

---
+++ Emoji.Wpf/Picker.xaml.cs
@@ -0,0 +1,71 @@
+﻿//
+//  Emoji.Wpf — Emoji support for WPF
+//
+//  Copyright © 2017—2018 Sam Hocevar <sam@hocevar.net>
+//
+//  This program is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What the Fuck You Want
+//  to Public License, Version 2, as published by the WTFPL Task Force.
+//  See http://www.wtfpl.net/ for more details.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Emoji.Wpf
+{
+    /// <summary>
+    /// Interaction logic for Picker.xaml
+    /// </summary>
+    public partial class Picker : StackPanel
+    {
+        public Picker()
+        {
+            InitializeComponent();
+        }
+
+        public IEnumerable<string> EmojiList { get => m_emoji_list; }
+
+        public double FontSize
+        {
+            get => TextBlock.FontSize;
+            set => TextBlock.FontSize = value;
+        }
+
+        public string Text
+        {
+            get => TextBlock.Text;
+            set => TextBlock.Text = value;
+        }
+
+        private static IEnumerable<string> m_emoji_list = Data.GetSortedEmoji();
+
+        private void OnEmojiSelected(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            TextBlock.Text = button.DataContext as string;
+            Button.IsChecked = false;
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(Picker), new PropertyMetadata("☺"));
+    }
+
+    public class BoolInverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool ? !(bool)value : value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool ? !(bool)value : value;
+    }
+}
+
