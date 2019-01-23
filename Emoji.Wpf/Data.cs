de8edca2594ded2c4f25d2520b0ce5baadc4c394
Sam Hocevar <sam@hocevar.net>
2019-01-23 13:47:21 +0100

    Release version 0.0.17 to NuGet.

--- Emoji.Wpf/Data.cs
+++ Emoji.Wpf/Data.cs
@@ -1,7 +1,7 @@
 ﻿//
 //  Emoji.Wpf — Emoji support for WPF
 //
-//  Copyright © 2017—2018 Sam Hocevar <sam@hocevar.net>
+//  Copyright © 2017—2019 Sam Hocevar <sam@hocevar.net>
 //
 //  This library is free software. It comes without any warranty, to
 //  the extent permitted by applicable law. You can redistribute it
