e5bd5b05648553311faa0fdda0f94452de69dede
Sam Hocevar <sam@hocevar.net>
2020-11-08 11:45:43 +0100

    RichTextBox improvements:
     - disable Ctrl-Z for now due to framework crashes
     - ensure Copy and Cut commands produce a clean clipboard content
     - add a Text property to capture all the document contents

--- Emoji.Wpf/RichTextBox.xaml.cs
+++ Emoji.Wpf/RichTextBox.xaml.cs
@@ -10,56 +10,113 @@
 //  See http://www.wtfpl.net/ for more details.
 //
 
-using System;
-using System.Text.RegularExpressions;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Markup;
 
 namespace Emoji.Wpf
 {
+    public sealed class TextSelection : TextRange
+    {
+        internal TextSelection(TextPointer start, TextPointer end)
+          : base(start, end) { }
+
+        /// <summary>
+        /// Override selection to text conversion in order to convert back all
+        /// EmojiInline instances to their equivalent UTF-8 sequences.
+        /// </summary>
+        public new string Text
+        {
+            get
+            {
+                var buf = new StringBuilder();
+
+                for (TextPointer p = Start, next = null;
+                     p != null && p.CompareTo(End) < 0;
+                     p = next)
+                {
+                    next = p.GetNextContextPosition(LogicalDirection.Forward);
+                    if (next == null)
+                        break;
+
+                    switch (p.GetPointerContext(LogicalDirection.Forward))
+                    {
+                        case TextPointerContext.ElementStart:
+                            if (p.GetAdjacentElement(LogicalDirection.Forward) is EmojiInline emoji)
+                                buf.Append(emoji.Text);
+                            break;
+                        case TextPointerContext.ElementEnd:
+                        case TextPointerContext.EmbeddedElement:
+                            break;
+                        case TextPointerContext.Text:
+                            // Get text from the Run but don’t go past end
+                            buf.Append(new TextRange(p, next.CompareTo(End) < 0 ? next : End).Text);
+                            break;
+                    }
+                }
+
+                return buf.ToString();
+            }
+        }
+    }
+
     public partial class RichTextBox : System.Windows.Controls.RichTextBox
     {
         public RichTextBox()
         {
+            CommandManager.AddPreviewExecutedHandler(this, PreviewExecuted);
             SetValue(Block.LineHeightProperty, 1.0);
-            DataObject.AddCopyingHandler(this, new DataObjectCopyingEventHandler(OnCopy));
+            Selection = new TextSelection(Document.ContentStart, Document.ContentStart);
         }
 
-        protected void OnCopy(object o, DataObjectCopyingEventArgs e)
+        protected override void OnSelectionChanged(RoutedEventArgs e)
         {
-            string clipboard = "";
+            base.OnSelectionChanged(e);
+            Selection = new TextSelection(base.Selection.Start, base.Selection.End);
+        }
 
-            for (TextPointer p = Selection.Start, next = null;
-                 p != null && p.CompareTo(Selection.End) < 0;
-                 p = next)
+        /// <summary>
+        /// Disable Ctrl-Z to prevent the widget internals from adding data to the
+        /// FlowDocument. This is unfortunate but there are too many crashes caused
+        /// by this. We probably need a custom undo/redo stack.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control && e.Key == Key.Z)
+                e.Handled = true;
+            else
+                base.OnPreviewKeyDown(e);
+        }
+
+        /// <summary>
+        /// Intercept Copy and Cut commands to make sure the clipboard contains the
+        /// proper emoji characters.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (sender is RichTextBox rtb)
             {
-                next = p.GetNextContextPosition(LogicalDirection.Forward);
-                if (next == null)
-                    break;
-
-                switch (p.GetPointerContext(LogicalDirection.Forward))
+                if (e.Command == ApplicationCommands.Copy || e.Command == ApplicationCommands.Cut)
                 {
-                    case TextPointerContext.ElementStart:
-                        if (p.GetAdjacentElement(LogicalDirection.Forward) is EmojiInline emoji)
-                            clipboard += emoji.Text;
-                        break;
-                    case TextPointerContext.ElementEnd:
-                    case TextPointerContext.EmbeddedElement:
-                        break;
-                    case TextPointerContext.Text:
-                        // Get text from the text run but don’t go past end
-                        clipboard += new TextRange(p, next.CompareTo(Selection.End) < 0 ? next : Selection.End).Text;
-                        break;
+                    var selection = rtb.Selection.Text;
+                    if (e.Command == ApplicationCommands.Cut)
+                        rtb.Cut();
+                    Clipboard.SetText(selection);
+                    e.Handled = true;
                 }
             }
-
-            Clipboard.SetText(clipboard);
-            e.Handled = true;
-            e.CancelCommand();
         }
 
+        /// <summary>
+        /// Replace Emoji characters with EmojiInline objects inside the document.
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnTextChanged(TextChangedEventArgs e)
         {
             if (m_pending_change)
@@ -104,9 +161,12 @@ namespace Emoji.Wpf
 
             m_pending_change = false;
 
-            // FIXME: this could be done on-demand by detecting GetValue() calls
+            // FIXME: this could be done on-demand by detecting GetValue() calls maybe
+            SetValue(TextProperty, new TextSelection(Document.ContentStart, Document.ContentEnd).Text);
+#if DEBUG
             var xaml = XamlWriter.Save(Document);
             SetValue(XamlTextProperty, xaml);
+#endif
         }
 
         private bool m_pending_change = false;
@@ -135,11 +195,21 @@ namespace Emoji.Wpf
             return ret;
         }
 
+        public new TextSelection Selection { get; private set; }
+
+        public string Text => (string)GetValue(TextProperty);
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text), typeof(string), typeof(RichTextBox),
+            new PropertyMetadata(""));
+
+#if DEBUG
         public string XamlText => (string)GetValue(XamlTextProperty);
 
         public static readonly DependencyProperty XamlTextProperty = DependencyProperty.Register(
             nameof(XamlText), typeof(string), typeof(RichTextBox),
             new PropertyMetadata(""));
+#endif
     }
 }
 
