4b06673925501361933215e7ed657c1b18218b57
Sam Hocevar <sam@hocevar.net>
2021-01-21 22:49:53 +0100

    More emoji:TextBox tests. Far from ready.

--- Emoji.Wpf/TextBox.xaml.cs
+++ Emoji.Wpf/TextBox.xaml.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Emoji.Wpf
@@ -25,27 +26,50 @@ namespace Emoji.Wpf
             InitializeComponent();
         }
 
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+#if DEBUG
+            Console.WriteLine($"Property Changed: {e.Property}");
+#endif
+            base.OnPropertyChanged(e);
+        }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+            m_rtb = Template.FindName("RichTextBox_INTERNAL", this) as RichTextBox;
 
             // Build a list of TextBox properties that are not inherited from Control. These
             // are the properties we want to bind to our child RichTextBox.
-            var exclude = GetReadWriteProperties(typeof(System.Windows.Controls.Control))
-                             .Select(x => x.Name).ToHashSet();
-            var propset = GetReadWriteProperties(typeof(TextBox))
-                             .Select(x => x.Name).ToHashSet();
+            var exclude = GetReadWritePropertyNames(typeof(System.Windows.Controls.Control));
+            var propset = GetReadWritePropertyNames(typeof(TextBox));
             propset.ExceptWith(exclude);
 
+#if DEBUG
+            var tmp1 = GetReadWritePropertyNames(typeof(RichTextBox));
+            tmp1.ExceptWith(exclude);
+            var tmp2 = propset.ToHashSet();
+            tmp2.ExceptWith(tmp1);
+            tmp1.ExceptWith(propset);
+
+            Console.WriteLine("RichTextBox properties not in TextBox: " + string.Join(" ", tmp1));
+            Console.WriteLine("TextBox properties not in RichTextBox: " + string.Join(" ", tmp2));
+#endif
+
+            // Add some Control properties that we want to inherit
+            propset.UnionWith(new List<string>()
+            {
+                "Foreground",
+            });
+
             // Iterate over all RichTextBox properties; for each found match, create a
-            // two-way binding with the TextBox element.
-            var rtb = Template.FindName("RichTextBox_INTERNAL", this) as RichTextBox;
+            // two-way binding with one of our properties.
             foreach (var dpd in GetReadWriteProperties(typeof(RichTextBox))
                                    .Where(x => propset.Contains(x.Name))
                                    .Select(x => DependencyPropertyDescriptor.FromProperty(x))
                                    .Where(x => x != null))
             {
-                rtb.SetBinding(dpd.DependencyProperty, new Binding(dpd.Name)
+                m_rtb.SetBinding(dpd.DependencyProperty, new Binding(dpd.Name)
                 {
                     Source = this,
                     Mode = BindingMode.TwoWay,
@@ -57,5 +81,10 @@ namespace Emoji.Wpf
             => TypeDescriptor.GetProperties(t, new Attribute[] { new PropertyFilterAttribute(PropertyFilterOptions.All) })
                              .Cast<PropertyDescriptor>()
                              .Where(x => !x.IsReadOnly);
+
+        private static HashSet<string> GetReadWritePropertyNames(Type t)
+            => GetReadWriteProperties(t).Select(x => x.Name).ToHashSet();
+
+        private RichTextBox m_rtb;
     }
 }
