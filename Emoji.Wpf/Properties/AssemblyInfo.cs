c6ae29583d3abac78acf8590dd86e242cdbdbd11
Sam Hocevar <sam@hocevar.net>
2021-01-22 13:52:16 +0100

    Use GitVersion for automatic versioning.
    Assembly and NuGet package versions are autodetected based on the
    last git tag. If the tag was e.g. "v0.2.0" then it will build a package
    with version 0.2.1. Exception: if the tag is on HEAD then the exact
    version, 0.2.0, is used instead.

--- Emoji.Wpf/Properties/AssemblyInfo.cs
+++ Emoji.Wpf/Properties/AssemblyInfo.cs
@@ -18,11 +18,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Emoji.Wpf")]
-[assembly: AssemblyDescription("Emoji support for WPF applications")]
+[assembly: AssemblyDescription("Emoji rendering for WPF applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Emoji.Wpf")]
-[assembly: AssemblyCopyright("Copyright © 2017—2020 Sam Hocevar")]
+[assembly: AssemblyCopyright("Copyright © 2017—2021 Sam Hocevar")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,15 +33,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6a9c0ad6-14f7-451a-a0d4-38987a275f0d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0")]
