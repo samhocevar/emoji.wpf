0175890f3001ebfd3aa03393ce77a1ea9a10323e
Sam Hocevar <sam@hocevar.net>
2021-01-28 22:19:24 +0100

    Make Behaviors.SmoothScrolling public.
    This code is duplicated in WinCompose, so I’m exposing it for convenience.

--- Emoji.Wpf/Behaviors.cs
+++ Emoji.Wpf/Behaviors.cs
@@ -17,36 +17,40 @@ using System.Windows.Controls;
 
 namespace Emoji.Wpf
 {
-    // Code is inspired by https://stackoverflow.com/a/17431815/111461
-    internal static class PixelBasedScrollingBehavior
+    public static class Behaviors
     {
-        internal static bool GetIsEnabled(DependencyObject o)
-            => (bool)o.GetValue(IsEnabledProperty);
+        /// <summary>
+        /// Attach this property to a VirtualizingStackPanel as well as its containing ListView
+        /// to enable per-pixel scrolling. Uses ScrollUnitProperty (.NET Framework 4.5 and later)
+        /// when available, otherwise uses the private VirtualizingPanel.IsPixelBased property
+        /// through reflexion. Code is inspired by https://stackoverflow.com/a/17431815/111461
+        /// </summary>
+        public static readonly DependencyProperty SmoothScrollingProperty =
+            DependencyProperty.RegisterAttached("SmoothScrolling", typeof(bool), typeof(Behaviors),
+                                                new UIPropertyMetadata(false, SmoothScrollingChanged));
 
-        internal static void SetIsEnabled(DependencyObject o, bool val)
-            => o.SetValue(IsEnabledProperty, val);
+        public static bool GetSmoothScrolling(DependencyObject o)
+            => (bool)o.GetValue(SmoothScrollingProperty);
 
-        internal static readonly DependencyProperty IsEnabledProperty =
-            DependencyProperty.RegisterAttached("IsEnabled", typeof(bool), typeof(PixelBasedScrollingBehavior),
-                                                new UIPropertyMetadata(false, IsEnabledChanged));
+        public static void SetSmoothScrolling(DependencyObject o, bool val)
+            => o.SetValue(SmoothScrollingProperty, val);
 
-        private static void IsEnabledChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        private static void SmoothScrollingChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
         {
             if (o is VirtualizingPanel || o is ItemsControl)
             {
-                var val = (bool)e.NewValue;
                 var enum_type = typeof(Window).Assembly.GetType("System.Windows.Controls.ScrollUnit");
                 var field_flags = BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy;
                 var field = typeof(VirtualizingPanel).GetField("ScrollUnitProperty", field_flags);
                 if (enum_type != null && field?.GetValue(null) is DependencyProperty dp)
                 {
-                    o.SetValue(dp, Enum.Parse(enum_type, val ? "Pixel" : "Item"));
+                    o.SetValue(dp, Enum.Parse(enum_type, (bool)e.NewValue ? "Pixel" : "Item"));
                 }
                 else if (o is VirtualizingPanel)
                 {
                     var prop_flags = BindingFlags.NonPublic | BindingFlags.Instance;
                     var prop = o.GetType().GetProperty("IsPixelBased", prop_flags);
-                    prop?.SetValue(o, val, null);
+                    prop?.SetValue(o, (bool)e.NewValue, null);
                 }
             }
         }
